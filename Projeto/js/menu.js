41401ab1658cac7d32d5c3c7b3e2aa933dda817b
Bernardo <108159072+koalaoke@users.noreply.github.com>
2023-11-20 00:36:10 +0000

    Version 0.8v

---
+++ Projeto/js/menu.js
@@ -0,0 +1,8 @@
+function changeMenu() {
+  var x = document.getElementById("ul-links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
